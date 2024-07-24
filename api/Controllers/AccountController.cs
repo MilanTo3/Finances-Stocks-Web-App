5fedc242ed6372127d98c3b79e755e8002f3c49b
Milan Tomin <61879338+MilanTo3@users.noreply.github.com>
2024-07-24 17:49:25 +0200

    login

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -22,11 +22,13 @@ namespace api.Controllers
 
         private readonly UserManager<AppUser> _user;
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signinManager;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
            _user = userManager;
            _tokenService = tokenService;
+           _signinManager = signInManager;
         }
 
         [HttpPost("register")]
@@ -52,6 +54,30 @@ namespace api.Controllers
             }catch(Exception ex){
                 return StatusCode(500, ex);
             }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _user.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if (user == null) return Unauthorized("Invalid username!");
+
+            var result = await _signinManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+
+            return Ok(
+                new NewUserDTO
+                {
+                    Username = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
         }
 
         
